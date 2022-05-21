cf4513634c57826c1eb046801caceb43cc05b09f
Luccas Rocigno <luccasrocigno@hotmail.com>
2022-05-21 17:06:10 -0300

    added random gaps between pipes and medals images

--- PipePair.lua
+++ PipePair.lua
@@ -14,6 +14,9 @@ PipePair = Class{}
 local GAP_HEIGHT = 90
 
 function PipePair:init(y)
+    -- calculate a gap height to each new pipe pair
+    thisGapHeight = GAP_HEIGHT + math.random(-10, 20)
+   
     -- flag to hold whether this pair has been scored (jumped through)
     self.scored = false
 
@@ -26,11 +29,12 @@ function PipePair:init(y)
     -- instantiate two pipes that belong to this pair
     self.pipes = {
         ['upper'] = Pipe('top', self.y),
-        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + GAP_HEIGHT)
+        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + thisGapHeight)
     }
 
     -- whether this pipe pair is ready to be removed from the scene
     self.remove = false
+
 end
 
 function PipePair:update(dt)
